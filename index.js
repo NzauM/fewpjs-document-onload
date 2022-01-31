cca380941e28a9e96f04b8c2025035d14bccf87c
mercy.waeni <mercy.waeni@calltronix.com>
2022-01-31 11:00:46 +0300

    complete on document load lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+    const cont = document.querySelector('p');
+    // cont.textContent = "JavaScript is so cool. It lets me add text to my page programmatically."
+    cont.textContent = "This is really cool!"
+})
